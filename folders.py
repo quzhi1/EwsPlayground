eacea8434ae0d01f352aabbde8fd7e7f78d220f7
Zhi Qu <quzhi65222714@gmail.com>
2024-04-16 17:59:22 -0700

    Rename

--- folders.py
+++ folders.py
@@ -1,4 +1,4 @@
-from exchangelib import Credentials, Account, Configuration, Folder
+from exchangelib import Credentials, Account, Configuration
 import os
 
 exchange_server = 'east.EXCH092.serverdata.net'
@@ -9,12 +9,10 @@ config = Configuration(server=exchange_server, credentials=credentials)
 account = Account(primary_smtp_address=username, config=config, access_type='delegate')
 
 # List all calendars
-python_created_calendar_id = None
-print("Primary Calendar Name:", account.calendar.name)
-print("Primary Calendar ID:", account.calendar.id)
+python_created_folder_id = None
 for folder in account.msg_folder_root.walk():
     if folder.folder_class == 'IPF.Note':
         print("Folder Name:", folder.name)
         print("Folder ID:", folder.id)
         if folder.name == "python created" or folder.name == "python created renamed":
-            python_created_calendar_id = folder.id
+            python_created_folder_id = folder.id
