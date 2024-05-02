8232dbc6ff409addb5da348647571282662a0049
Zhi Qu <quzhi65222714@gmail.com>
2024-05-02 15:02:14 -0700

    Downgrade exchangelib

--- folders.py
+++ folders.py
@@ -9,10 +9,7 @@ config = Configuration(server=exchange_server, credentials=credentials)
 account = Account(primary_smtp_address=username, config=config, access_type='delegate')
 
 # List all calendars
-python_created_folder_id = None
 for folder in account.msg_folder_root.walk():
     if folder.folder_class == 'IPF.Note':
         print("Folder Name:", folder.name)
         print("Folder ID:", folder.id)
-        if folder.name == "python created" or folder.name == "python created renamed":
-            python_created_folder_id = folder.id
