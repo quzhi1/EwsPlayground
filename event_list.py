359d6eb2cef77b4ce1e7a537b7717e00e523aaee
Zhi Qu <quzhi65222714@gmail.com>
2024-05-06 14:26:58 -0700

    Cleanup

--- event_list.py
+++ event_list.py
@@ -10,10 +10,11 @@ config = Configuration(server=exchange_server, credentials=credentials)
 account = Account(primary_smtp_address=username, config=config, access_type='delegate')
 
 # Define the start and end times for the query
+tz = EWSTimeZone("America/New_York")
 now = datetime.now()
 end_time = now + timedelta(days=30)
-start = EWSDateTime(now.year, now.month, now.day, tzinfo=EWSTimeZone.localzone())
-end = EWSDateTime(end_time.year, end_time.month, end_time.day, tzinfo=EWSTimeZone.localzone())
+start = EWSDateTime(now.year, now.month, now.day, tzinfo=tz)
+end = EWSDateTime(end_time.year, end_time.month, end_time.day, tzinfo=tz)
 
 # Query for primary calendar events
 calendar_items = account.calendar.view(start=start, end=end)
@@ -40,14 +41,17 @@ for item in calendar_items:
 # Query for free-busy information
 # attendee_type can be "Optional", "Organizer", "Required", "Resource", "Room"
 accounts = [
-    ("test@nylas.info", "Required", False),
     ("staging_test@nylas.info", "Required", False),
+    ("test@nylas.info", "Required", False),
 ]
-free_busy = account.protocol.get_free_busy_info(accounts=accounts, start=start, end=end, merged_free_busy_interval=30)
+free_busy = account.protocol.get_free_busy_info(accounts=accounts, start=start, end=end, merged_free_busy_interval=30, requested_view='FreeBusyMerged')
 print("Free-busy information:")
+progress = 0
+print("free_busy:", f"{type(free_busy).__module__}.{type(free_busy).__name__}")
 for busy_info in free_busy:
-    print("working_hours:", busy_info.working_hours)
-    if busy_info.calendar_events is None:
-        continue
-    for event in busy_info.calendar_events:
-        print("busy_type:", event.busy_type, "start:", event.start)
+    print("working_hours:", busy_info.working_hours, "email:", accounts[progress][0])
+    if busy_info.calendar_events is not None:
+        for event in busy_info.calendar_events:
+            print("busy_type:", event.busy_type, "start:",
+                  event.start, "end:", event.end)
+    progress += 1
