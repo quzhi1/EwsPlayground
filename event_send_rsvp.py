55cbe598567c4130818d38edee075824abc09d7a
Zhi Qu <quzhi65222714@gmail.com>
2024-05-07 16:45:38 -0700

    Send rsvp

--- event_send_rsvp.py
+++ event_send_rsvp.py
@@ -1,4 +1,4 @@
-from exchangelib import Credentials, Account, Configuration, EWSDateTime, EWSTimeZone
+from exchangelib import Credentials, Account, Configuration, EWSDateTime, EWSTimeZone, CalendarItem
 from exchangelib.items import SEND_ONLY
 from datetime import datetime, timedelta
 import os
@@ -19,16 +19,17 @@ start = EWSDateTime(now.year, now.month, now.day, tzinfo=tz)
 end = EWSDateTime(end_time.year, end_time.month, end_time.day, tzinfo=tz)
 
 # Query for primary calendar events
-# calendar_items = account.calendar.view(start=start, end=end)
-# print("Primary calendar events:")
-# for item in calendar_items:
-#     if isinstance(item, CalendarItem):
-#         item = account.calendar.get(id=item.id)
-#         print("subject:", item.subject, "start:", item.start, "id:", item.id)
+calendar_items = account.calendar.view(start=start, end=end)
+print("Primary calendar events:")
+for item in calendar_items:
+    if isinstance(item, CalendarItem):
+        item = account.calendar.get(id=item.id)
+        print("subject:", item.subject, "start:", item.start, "id:", item.id)
 
 # Respond to an event
-event_id = 'AAMkAGY0ODk4NjA1LTc5MWQtNDE0NS1iMmJkLTk5YzYxZjk2NzY5YQFRAAgI3HCEIrRAAEYAAAAAAP1kvVnViEqVb/Ymc6I43gcAqJTRI/07z06lEfGhzXbTqwAAAAABDQAAqJTRI/07z06lEfGhzXbTqwADJR1y3AAAEA=='
+event_id = 'AAMkAGY0ODk4NjA1LTc5MWQtNDE0NS1iMmJkLTk5YzYxZjk2NzY5YQFRAAgI3HCEIrRAAEYAAAAAAP1kvVnViEqVb/Ymc6I43gcAqJTRI/07z06lEfGhzXbTqwAAAAABDQAAqJTRI/07z06lEfGhzXbTqwADJR1y3wAAEA=='
 event = account.calendar.get(id=event_id)
-# event.accept(message_disposition=SEND_ONLY)
+returned = event.accept(message_disposition=SEND_ONLY)
+print(f"{returned.__module__}.BulkCreateResult", returned)
 # event.tentatively_accept(message_disposition=SEND_ONLY)
-event.decline(message_disposition=SEND_ONLY)
+# event.decline(message_disposition=SEND_ONLY)
