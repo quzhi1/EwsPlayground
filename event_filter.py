5f7d2f2875dd44f665e709f1734a1707dc3bf7f7
Zhi Qu <quzhi65222714@gmail.com>
2024-06-18 11:00:40 -0700

    filter event by uid

--- event_filter.py
+++ event_filter.py
@@ -1,5 +1,5 @@
-from exchangelib import Credentials, Account, Configuration, EWSDateTime, EWSTimeZone, CalendarItem
-from datetime import datetime, timedelta
+from exchangelib import Credentials, Account, Configuration, CalendarItem
+from exchangelib.properties import UID
 import os
 
 exchange_server = 'east.EXCH092.serverdata.net'
@@ -11,7 +11,24 @@ account = Account(primary_smtp_address=username,
                   config=config, access_type='delegate')
 
 # Find event by uid
-uid = "040000008200E00074C5B7101A82E008000000007FB4D296AA9DDA010000000000000000100000002BAD9240C65B9440AD614916D81C3D72"
-event = account.calendar.get(uid=uid)
-print("Event by uid:", event.subject, event.start, event.id)
+uid = "040000008200E00074C5B7101A82E008000000009983D9D2A4C1DA0100000000000000001000000077D48B2F31EE2147B12CB9CE7FFAC178"
+# uid = "abcde"
+
 # EWS does not support filtering on field 'uid'
+
+# items = account.calendar.filter(Q(uid=uid))
+# for item in items:
+#     print(f'Subject: {item.subject}')
+#     print(f'Start: {item.start}')
+#     print(f'End: {item.end}')
+#     print(f'Location: {item.location}')
+#     print(f'UID: {item.uid}')
+
+uid = UID(UID.to_global_object_id(uid))
+print("unhexlified uid:", uid)
+event = account.calendar.all().only('global_object_id').get(global_object_id=uid)
+print("Event by uid:", event.subject, event.start, event.id)
+
+# for event in account.calendar.all().only("id", "uid"):
+#     if isinstance(event, CalendarItem) and event.uid == uid:
+#         print("subject:", event.subject, "start:", event.start, "id:", event.id)
