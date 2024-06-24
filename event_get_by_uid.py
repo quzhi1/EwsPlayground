480b1b15361c936b88f97ba136a65ca5fd6c0ab9
Zhi Qu <quzhi65222714@gmail.com>
2024-06-24 11:20:27 -0700

    Update script to get event by uid

--- event_get_by_uid.py
+++ event_get_by_uid.py
@@ -2,6 +2,7 @@ from exchangelib import Credentials, Account, Configuration, CalendarItem
 from exchangelib.properties import UID
 from exchangelib.extended_properties import ExtendedProperty
 import os
+import uuid
 
 exchange_server = 'east.EXCH092.serverdata.net'
 username = os.getenv('INTERMEDIA_EMAIL_ADDRESS')
@@ -20,8 +21,9 @@ class GlobalObjectId(ExtendedProperty):
 CalendarItem.register("global_object_id", GlobalObjectId)
 
 # Find event by uid
-uid = "040000008200E00074C5B7101A82E008000000009983D9D2A4C1DA0100000000000000001000000077D48B2F31EE2147B12CB9CE7FFAC178"
+uid = "040000008200E00074C5B7101A82E0080000000069E26FC662C6DA01000000000000000010000000E912B5F2D12C544FADE443C0D0C6E3EE"
 # uid = "abcde"
 uid = UID.to_global_object_id(uid)
 event = account.calendar.all().get(global_object_id=uid)
-print("Event by uid:", event.subject, event.start, event.id)
+print("Subject:", event.subject, "start:", event.start, "id:",
+      event.id, "uid:", event.uid)
