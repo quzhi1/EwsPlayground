e4a3326dde8759d35ca128664877efe32ce9823b
Zhi Qu <quzhi65222714@gmail.com>
2024-07-29 17:21:50 -0700

    Autodiscovery done

--- autodiscovery.py
+++ autodiscovery.py
@@ -1,11 +1,17 @@
 from exchangelib import Credentials, autodiscover
 import os
 
-cred = Credentials(username=os.getenv('EWS_OVH_USERNAME'),
-                   password=os.getenv('EWS_OVH_PASSWORD'))
-result, protocol = autodiscover.discover(os.getenv('EWS_OVH_USERNAME'), cred)
-print("user_display_name:", result.user_settings['user_display_name'])
-print("smtp_address:", result.user_settings['auto_discover_smtp_address'])
-print("ews_supported_schemas:", result.user_settings['ews_supported_schemas'])
-print("service_endpoint:", protocol.config.service_endpoint)
-print("version:", protocol.config.version)
+# username = os.getenv('EWS_OVH_USERNAME')
+# password = os.getenv('EWS_OVH_PASSWORD')
+username = os.getenv('INTERMEDIA_EMAIL_ADDRESS')
+password = os.getenv('INTERMEDIA_PASSWORD')
+cred = Credentials(username=username, password=password)
+try:
+    result, protocol = autodiscover.discover(username, cred)
+    print("user_display_name:", result.user_settings['user_display_name'])
+    print("smtp_address:", result.user_settings['auto_discover_smtp_address'])
+    print("ews_supported_schemas:", result.user_settings['ews_supported_schemas'])
+    print("service_endpoint:", protocol.config.service_endpoint)
+    print("version:", protocol.config.version)
+except Exception as e:
+    print(e)
