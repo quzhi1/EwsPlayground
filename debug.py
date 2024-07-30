9434d332b3c4c9df46f16b9f105ca3c4c13bcf91
Zhi Qu <quzhi65222714@gmail.com>
2024-07-30 16:08:46 -0700

    Immo account not behaving as expected

--- debug.py
+++ debug.py
@@ -7,9 +7,12 @@ logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('exchangelib').setLevel(logging.DEBUG)
 logging.getLogger('requests').setLevel(logging.DEBUG)
 
-exchange_server = 'ex.mail.ovh.ca'
-username = os.getenv('EWS_OVH_USERNAME')
-password = os.getenv('EWS_OVH_PASSWORD')
+# exchange_server = 'ex.mail.ovh.ca'
+# username = os.getenv('EWS_OVH_USERNAME')
+# password = os.getenv('EWS_OVH_PASSWORD')
+exchange_server = 'zimbra.immo-facile.com'
+username = os.getenv('EWS_IMMO_USERNAME')
+password = os.getenv('EWS_IMMO_PASSWORD')
 credentials = Credentials(username=username, password=password)
 config = Configuration(server=exchange_server, credentials=credentials)
 account = Account(primary_smtp_address=username,
