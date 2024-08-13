a740743cdde7a193317eb0e0a4867ed2a86c5e13
Zhi Qu <quzhi65222714@gmail.com>
2024-08-13 10:01:47 -0700

    Zimbra update

--- zimbra.py
+++ zimbra.py
@@ -16,36 +16,41 @@ class Build(_Build):
         except ValueError:
             pass
 
+
 # exchange_server = 'ex.mail.ovh.ca'
 # username = os.getenv('EWS_OVH_USERNAME')
 # password = os.getenv('EWS_OVH_PASSWORD')
-exchange_server = 'zimbra.immo-facile.com'
-username = os.getenv('EWS_IMMO_USERNAME')
-password = os.getenv('EWS_IMMO_PASSWORD')
+# exchange_server = 'zimbra.immo-facile.com'
+# username = os.getenv('EWS_IMMO_USERNAME')
+# password = os.getenv('EWS_IMMO_PASSWORD')
+exchange_server = 'zimbra.xmission.com'
+username = os.getenv('EWS_ZIMBRA_USERNAME')
+password = os.getenv('EWS_ZIMBRA_PASSWORD')
 credentials = Credentials(
     username=username,
     password=password,
 )
-config = Configuration(
-    server=exchange_server,
-    credentials=credentials,
-    # Add version hint to skip version guessing routines
-    version=Version(
-        api_version="Version366750107",
-        build=Build(
-            major_version=-1102247790,
-            minor_version=-801581130,
-            major_build=-1919954918,
-            minor_build=-880192749,
-        ),
-    ),
-)
+# config = Configuration(
+#     server=exchange_server,
+#     credentials=credentials,
+#     # Add version hint to skip version guessing routines
+#     version=Version(
+#         api_version="Version366750107",
+#         build=Build(
+#             major_version=-1102247790,
+#             minor_version=-801581130,
+#             major_build=-1919954918,
+#             minor_build=-880192749,
+#         ),
+#     ),
+# )
+config = Configuration(server=exchange_server, credentials=credentials)
 account = Account(
     primary_smtp_address=username,
     config=config,
     access_type='delegate',
     # Disable autodiscover to skip version guessing routines
-    autodiscover=False,
+    # autodiscover=False,
 )
 
 # print("Primary Calendar Name:", account.calendar.name)
