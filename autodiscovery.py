d8a6984ae1d75dc3d343c50ca0630a4f0725c636
Zhi Qu <quzhi65222714@gmail.com>
2024-10-02 17:11:37 -0700

    OVH

--- autodiscovery.py
+++ autodiscovery.py
@@ -1,8 +1,9 @@
-from exchangelib import Credentials, autodiscover
+from exchangelib import Credentials, autodiscover, Configuration, Account, NTLM, BASIC, DIGEST
+from exchangelib.errors import UnauthorizedError, TransportError
 import os
 
-username = os.getenv('INTERMEDIA_EMAIL_ADDRESS')
-password = os.getenv('INTERMEDIA_PASSWORD')
+username = os.getenv('OVH_EMAIL_ADDRESS')
+password = os.getenv('OVH_PASSWORD')
 cred = Credentials(username=username, password=password)
 try:
     result, protocol = autodiscover.discover(username, cred)
@@ -13,5 +14,25 @@ try:
     print("version.build:", protocol.config.version.build)
     print("version.api_version:", protocol.config.version.api_version)
     print("version.fullname:", protocol.config.version.fullname)
+
+    for auth_type in [NTLM, BASIC, DIGEST]:
+        server_endpoint_without_scheme = protocol.config.service_endpoint.split('://')[1]
+        config = Configuration(
+            server=server_endpoint_without_scheme,
+            credentials=cred,
+            auth_type=auth_type,
+            # retry_policy=FaultTolerance(
+            #     max_wait=os.getenv('EWS_TIMEOUT', 10),
+            # ),
+        )
+        try:
+            exlib_account = Account(
+                primary_smtp_address=username, config=config, access_type='delegate')
+            print(auth_type)
+            break
+        except (UnauthorizedError, TransportError):
+            continue
+
 except Exception as e:
     print(e)
+
