acb91e9f612e5a647b564cd8c80ba02e67bc8f4f
Zhi Qu <quzhi65222714@gmail.com>
2024-11-11 15:03:02 -0800

    Debug another one

--- debugging_find_all_items_folder.py
+++ debugging_find_all_items_folder.py
@@ -4,12 +4,17 @@ import os
 # exchange_server = 'east.EXCH092.serverdata.net'
 # username = os.getenv('INTERMEDIA_EMAIL_ADDRESS')
 # password = os.getenv('INTERMEDIA_PASSWORD')
-exchange_server = 'exch.myexchangeemail.com'
-username = os.getenv('MYEXCHANGEEMAIL_EMAIL_ADDRESS')
-password = os.getenv('MYEXCHANGEEMAIL_PASSWORD')
+# exchange_server = 'exch.myexchangeemail.com'
+# username = os.getenv('MYEXCHANGEEMAIL_EMAIL_ADDRESS')
+# password = os.getenv('MYEXCHANGEEMAIL_PASSWORD')
+exchange_server = 'webmail.mollundpunt.at'
+username = os.getenv('MOLLUNDPUNT_USERNAME')
+email = os.getenv('MOLLUNDPUNT_EMAIL_ADDRESS')
+password = os.getenv('MOLLUNDPUNT_PASSWORD')
+print(username, email, password)
 credentials = Credentials(username=username, password=password)
 config = Configuration(server=exchange_server, credentials=credentials)
-account = Account(primary_smtp_address=username,
+account = Account(primary_smtp_address=email,
                   config=config, access_type='delegate')
 
 # print(account.root / 'AllItems')
