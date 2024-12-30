2b27d8b9c21f0ee4d4c128ab6221b1c2ba4b6396
Zhi Qu <quzhi65222714@gmail.com>
2024-12-30 10:25:09 -0800

    List messages

--- list_messages.py
+++ list_messages.py
@@ -1,4 +1,4 @@
-from exchangelib import Credentials, Account, Configuration
+from exchangelib import Credentials, Account, Configuration, ExtendedProperty, Message
 from exchangelib.folders import AllItems
 import os
 
@@ -10,9 +10,42 @@ config = Configuration(server=exchange_server, credentials=credentials)
 account = Account(primary_smtp_address=username,
                   config=config, access_type='delegate')
 
+class Flag(ExtendedProperty):
+    property_tag = 0x1090
+    property_type = 'Integer'
+
+Message.register('flag', Flag)
+
+NECESSARY_MESSAGE_FIELDS_WITHOUT_MIME = [
+    "id",
+    "message_id",
+    "conversation_id",
+    "parent_folder_id",
+    "subject",
+    "in_reply_to",
+    "author",
+    "to_recipients",
+    "cc_recipients",
+    "bcc_recipients",
+    "reply_to",
+    "body",
+    "attachments",
+    "is_read",
+    "flag",
+    "is_draft",
+    "has_attachments",
+    "headers",
+    "conversation_index",
+    "datetime_received",
+    "datetime_sent",
+    "datetime_created",
+    "last_modified_time",
+    "item_class",
+]
+
 def main():
     all_items_folder = account.root.get_default_folder(AllItems)
-    messages = all_items_folder.all().order_by('-datetime_received').only('subject', 'datetime_received', 'is_draft')
+    messages = all_items_folder.all().order_by('-datetime_received').only(*NECESSARY_MESSAGE_FIELDS_WITHOUT_MIME)
 
     for message in messages[0:10]:
         print('subject:', message.subject)
