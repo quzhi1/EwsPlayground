d1b6eab4980cbebe0b84194fcfaa2de5789c3073
Zhi Qu <quzhi65222714@gmail.com>
2025-01-09 13:40:50 -0800

    Debug calendar

--- calendars.py
+++ calendars.py
@@ -3,57 +3,62 @@ from exchangelib.folders import SingleFolderQuerySet, Calendar
 from exchangelib.properties import DistinguishedFolderId
 import os
 
-exchange_server = 'east.EXCH092.serverdata.net'
-username = os.getenv('INTERMEDIA_EMAIL_ADDRESS')
-password = os.getenv('INTERMEDIA_PASSWORD')
+# exchange_server = 'east.EXCH092.serverdata.net'
+# username = os.getenv('INTERMEDIA_EMAIL_ADDRESS')
+# password = os.getenv('INTERMEDIA_PASSWORD')
+exchange_server = 'mail.lobelfinancial.com'
+username = os.getenv('LOBELFINANCIAL_USERNAME')
+email_address = os.getenv('LOBELFINANCIAL_EMAIL_ADDRESS')
+password = os.getenv('LOBELFINANCIAL_PASSWORD')
 credentials = Credentials(username=username, password=password)
 config = Configuration(server=exchange_server, credentials=credentials)
-account = Account(primary_smtp_address=username, config=config, access_type='delegate')
+account = Account(primary_smtp_address=email_address, config=config, access_type='delegate')
 
 # List all calendars
-python_created_calendar_id = None
+# python_created_calendar_id = None
+python_created_calendar_id = 'AAMkADdhNWY1NjBkLTI2OGEtNDVjYy1iMDIwLWQxN2I0NGRkODU1ZAAuAAAAAACxwMOoDcdERbBsU78FkwzEAQBrZ4Sew6wlTIDsxcP3+cA4B9J9zHfEAAA='
 print("Primary Calendar Name:", account.calendar.name)
 print("Primary Calendar ID:", account.calendar.id)
 for folder in account.calendar.walk():
     if folder.folder_class == 'IPF.Appointment':
         print("Secondary Calendar Name:", folder.name)
         print("Secondary Calendar ID:", folder.id)
-        if folder.name == "python created" or folder.name == "python created renamed":
-            python_created_calendar_id = folder.id
+        # if folder.name == "python created" or folder.name == "python created renamed":
+        #     python_created_calendar_id = folder.id
 
-# Create calendar
-if python_created_calendar_id is None:
-    new_calendar = Folder(parent=account.calendar, name="python created", folder_class='IPF.Appointment')
-    new_calendar.save()
-    python_created_calendar_id = new_calendar.id
-    print("Created calendar, name:", new_calendar.name, "ID:", new_calendar.id)
+# # Create calendar
+# if python_created_calendar_id is None:
+#     new_calendar = Folder(parent=account.calendar, name="python created", folder_class='IPF.Appointment')
+#     new_calendar.save()
+#     python_created_calendar_id = new_calendar.id
+#     print("Created calendar, name:", new_calendar.name, "ID:", new_calendar.id)
 
-# Find calendar by id
+# # Find calendar by id
 find_folder_result = SingleFolderQuerySet(
     account=account,
     folder=account.calendar,
 ).get(id=python_created_calendar_id)
 print("Find folder by id result:", find_folder_result.name)
 
-# Find calendar by distinguished folder id
-find_folder_by_distinguished_folder_id_result = SingleFolderQuerySet(
-    account=account,
-    folder=DistinguishedFolderId(
-        id=Calendar.DISTINGUISHED_FOLDER_ID,
-    ),
-).resolve()
-print(
-    "Find folder by distinguished folder id result:",
-    find_folder_by_distinguished_folder_id_result.name,
-    "folder_class:",
-    find_folder_by_distinguished_folder_id_result.folder_class,
-)
+# # Find calendar by distinguished folder id
+# find_folder_by_distinguished_folder_id_result = SingleFolderQuerySet(
+#     account=account,
+#     folder=DistinguishedFolderId(
+#         id=Calendar.DISTINGUISHED_FOLDER_ID,
+#     ),
+# ).resolve()
+# print(
+#     "Find folder by distinguished folder id result:",
+#     find_folder_by_distinguished_folder_id_result.name,
+#     "folder_class:",
+#     find_folder_by_distinguished_folder_id_result.folder_class,
+# )
 
-# Rename calendar
-find_folder_result.name = "python created renamed"
-find_folder_result.save(update_fields=['name'])
-print("Folder renamed")
+# # Rename calendar
+# find_folder_result.name = "python created renamed"
+# find_folder_result.save(update_fields=['name'])
+# print("Folder renamed")
 
-# Delete calendar
-find_folder_result.delete()
-print("Folder deleted")
+# # Delete calendar
+# find_folder_result.delete()
+# print("Folder deleted")
