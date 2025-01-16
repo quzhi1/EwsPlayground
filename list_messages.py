bd1949688dd125ec0b6f83cffab8137da6c15c17
Zhi Qu <quzhi65222714@gmail.com>
2025-01-15 17:10:57 -0800

    Filter by conversation id

--- list_messages.py
+++ list_messages.py
@@ -1,5 +1,6 @@
 from exchangelib import Credentials, Account, Configuration, ExtendedProperty, Message, Q
 from exchangelib.folders import AllItems
+from exchangelib.properties import ConversationId
 import os
 
 exchange_server = 'west.EXCH092.serverdata.net'
@@ -47,21 +48,16 @@ def main():
     all_items_folder = account.root.get_default_folder(AllItems)
     # messages = all_items_folder.filter(item_class='IPM.Note').order_by('-datetime_received').only(*NECESSARY_MESSAGE_FIELDS_WITHOUT_MIME)
     # messages = all_items_folder.all().order_by('-datetime_received').only(*NECESSARY_MESSAGE_FIELDS_WITHOUT_MIME)
-    messages = all_items_folder.filter(Q(), is_draft=False, item_class='IPM.Note').order_by('-datetime_received').only(*NECESSARY_MESSAGE_FIELDS_WITHOUT_MIME)
-    # aqs = Q(author__icontains='zhi.q@nylas.com', has_attachments=False) & ~Q(flag=2)
-    # messages = all_items_folder.filter(
-    #     aqs,
-    #     is_draft=False,
-    #     item_class='IPM.Note',
-    #     ).order_by('-datetime_received').only(*NECESSARY_MESSAGE_FIELDS_WITHOUT_MIME)
+    # messages = all_items_folder.filter(Q(), is_draft=False, item_class='IPM.Note').order_by('-datetime_received').only(*NECESSARY_MESSAGE_FIELDS_WITHOUT_MIME)
+    aqs = Q(author__icontains='zhi.q@nylas.com', has_attachments=False) & ~Q(flag=2)
+    query = aqs & Q(is_draft=False, item_class='IPM.Note')
+    query = query & Q(conversation_id=ConversationId(id='AAQkADZhMzdkMDEzLWU1YTgtNDdiOC04ZmY4LTA4NWIzY2YzZTY1NQAQAImvnrfwEytJmqMLIPVAUKM='))
+    messages = all_items_folder.filter(query).order_by('-datetime_received').only(*NECESSARY_MESSAGE_FIELDS_WITHOUT_MIME)
 
     for message in messages[0:10]:
-        print('item_class:', message.item_class)
-        print('object_type:', type(message).__name__)
         print('subject:', message.subject)
-        print('datetime_received:', message.datetime_received)
-        print('is_draft', message.is_draft)
         print('sender', message.author.email_address)
+        print('conversation_id', message.conversation_id.id)
 
 if __name__ == '__main__':
     main()
