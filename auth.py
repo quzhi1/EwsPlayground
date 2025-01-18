f6a76df777ad8305e435a53eeacff4d5b264fd1b
Zhi Qu <quzhi65222714@gmail.com>
2025-01-17 16:27:56 -0800

    Papara

--- auth.py
+++ auth.py
@@ -5,16 +5,20 @@ import os
 # email = os.getenv('ROSELLILAW_EMAIL_ADDRESS')
 # username = 'Rosellilaw\\rmroselli'
 # password = os.getenv('ROSELLILAW_PASSWORD')
-exchange_server = 'webmail.wolve.com'
-username = os.getenv('WOLVE_EMAIL_ADDRESS')
-email = os.getenv('WOLVE_EMAIL_ADDRESS')
-password = os.getenv('WOLVE_PASSWORD')
+# exchange_server = 'webmail.wolve.com'
+# username = os.getenv('WOLVE_EMAIL_ADDRESS')
+# email = os.getenv('WOLVE_EMAIL_ADDRESS')
+# password = os.getenv('WOLVE_PASSWORD')
+exchange_server = 'mail.papara.com'
+username = os.getenv('PAPARA_EMAIL_ADDRESS')
+email = os.getenv('PAPARA_EMAIL_ADDRESS')
+password = os.getenv('PAPARA_PASSWORD')
 print(username, email, password)
 credentials = Credentials(username=username, password=password)
 config = Configuration(server=exchange_server,
                        credentials=credentials, 
                     #    auth_type=NTLM,
-                       )
+)
 account = Account(primary_smtp_address=email,
                   config=config, access_type='delegate')
 
