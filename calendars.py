ae5dc4bba3724fe9f0ab43040bba1c07bcd59655
Zhi Qu <quzhi65222714@gmail.com>
2025-02-12 10:41:28 -0800

    Calendar permissions

--- calendars.py
+++ calendars.py
@@ -3,26 +3,29 @@ from exchangelib.folders import SingleFolderQuerySet, Calendar
 from exchangelib.properties import DistinguishedFolderId
 import os
 
-# exchange_server = 'east.EXCH092.serverdata.net'
-# username = os.getenv('INTERMEDIA_EMAIL_ADDRESS')
-# password = os.getenv('INTERMEDIA_PASSWORD')
-exchange_server = 'mail.lobelfinancial.com'
-username = os.getenv('LOBELFINANCIAL_USERNAME')
-email_address = os.getenv('LOBELFINANCIAL_EMAIL_ADDRESS')
-password = os.getenv('LOBELFINANCIAL_PASSWORD')
+exchange_server = 'east.EXCH092.serverdata.net'
+username = os.getenv('YODA_EMAIL_ADDRESS')
+email_address = os.getenv('YODA_EMAIL_ADDRESS')
+password = os.getenv('YODA_PASSWORD')
 credentials = Credentials(username=username, password=password)
 config = Configuration(server=exchange_server, credentials=credentials)
 account = Account(primary_smtp_address=email_address, config=config, access_type='delegate')
 
 # List all calendars
 # python_created_calendar_id = None
-python_created_calendar_id = 'AAMkADdhNWY1NjBkLTI2OGEtNDVjYy1iMDIwLWQxN2I0NGRkODU1ZAAuAAAAAACxwMOoDcdERbBsU78FkwzEAQBrZ4Sew6wlTIDsxcP3+cA4B9J9zHfEAAA='
+# python_created_calendar_id = 'AAMkADdhNWY1NjBkLTI2OGEtNDVjYy1iMDIwLWQxN2I0NGRkODU1ZAAuAAAAAACxwMOoDcdERbBsU78FkwzEAQBrZ4Sew6wlTIDsxcP3+cA4B9J9zHfEAAA='
 print("Primary Calendar Name:", account.calendar.name)
 print("Primary Calendar ID:", account.calendar.id)
+print("Primary Calendar create permission:", account.calendar.effective_rights.create_contents if account.calendar.effective_rights else False)
+print("Primary Calendar modify permission:", account.calendar.effective_rights.modify if account.calendar.effective_rights else False)
+print("Primary Calendar delete permission:", account.calendar.effective_rights.delete if account.calendar.effective_rights else False)
 for folder in account.calendar.walk():
     if folder.folder_class == 'IPF.Appointment':
         print("Secondary Calendar Name:", folder.name)
         print("Secondary Calendar ID:", folder.id)
+        print("Secondary Calendar create permission:", folder.effective_rights.create_contents if folder.effective_rights else False)
+        print("Secondary Calendar modify permission:", folder.effective_rights.modify if folder.effective_rights else False)
+        print("Secondary Calendar delete permission:", folder.effective_rights.delete if folder.effective_rights else False)
         # if folder.name == "python created" or folder.name == "python created renamed":
         #     python_created_calendar_id = folder.id
 
@@ -34,11 +37,11 @@ for folder in account.calendar.walk():
 #     print("Created calendar, name:", new_calendar.name, "ID:", new_calendar.id)
 
 # # Find calendar by id
-find_folder_result = SingleFolderQuerySet(
-    account=account,
-    folder=account.calendar,
-).get(id=python_created_calendar_id)
-print("Find folder by id result:", find_folder_result.name)
+# find_folder_result = SingleFolderQuerySet(
+#     account=account,
+#     folder=account.calendar,
+# ).get(id=python_created_calendar_id)
+# print("Find folder by id result:", find_folder_result.name)
 
 # # Find calendar by distinguished folder id
 # find_folder_by_distinguished_folder_id_result = SingleFolderQuerySet(
