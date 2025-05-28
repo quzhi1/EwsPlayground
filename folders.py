91a8776f9908f9ae2caca7de1fad9045ea4c5b16
Zhi Qu <quzhi65222714@gmail.com>
2025-05-27 18:30:28 -0700

    Debugging ErrorNonExistentMailbox

--- folders.py
+++ folders.py
@@ -1,9 +1,9 @@
 from exchangelib import Credentials, Account, Configuration
 import os
 
-exchange_server = 'east.EXCH092.serverdata.net'
-username = os.getenv('INTERMEDIA_EMAIL_ADDRESS')
-password = os.getenv('INTERMEDIA_PASSWORD')
+exchange_server = 'webmail.wb-duisburg.de'
+username = os.getenv('DUISBURG_EMAIL_ADDRESS')
+password = os.getenv('DUISBURG_PASSWORD')
 credentials = Credentials(username=username, password=password)
 config = Configuration(server=exchange_server, credentials=credentials)
 account = Account(primary_smtp_address=username, config=config, access_type='delegate')
